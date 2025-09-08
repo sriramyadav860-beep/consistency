6045ef75545799fceb688fa884b01ec26540e309
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-09-08 22:19:11 +0530

    Your commit message

---
+++ datatypes.py
@@ -0,0 +1,23 @@
+decimal_num = 5.5
+integer_num = int(decimal_num)
+print("decimal",decimal_num)
+print("integer",integer_num)
+
+a = 5.4
+b = int(a)
+print("deci",a)
+print("int",b)
+
+a = "sriram"
+b = 7
+c = 5.5
+print("the type of 'a",type(a))
+print("type 'b",type(b))
+print("type 'c",type(c))
+
+
+a = "100"
+b = int(a)
+result = b + 50
+print(result)
+
