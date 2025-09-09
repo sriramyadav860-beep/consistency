ac104b21db9f8ea0fd7424a0198e5ad172cec3d9
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-09-09 23:14:39 +0530

    Add files via upload

---
+++ varibles.py
@@ -0,0 +1,41 @@
+a = 5;
+b = 5;
+print(a+b)
+
+name = "sriram"
+print("Name",name)
+
+
+num = 10
+print("Initial value (integer):", num)
+num = 10.5
+print("New value (float):", num)
+
+R = A = M = 3
+print("R",R)
+print("A",A)
+print("M",M)
+
+a=10
+b=5
+print("a",b)
+print("b",a)
+
+a,b=b,a 
+print("a",a)
+print("b",b)
+
+student1, student2, student3 = "sriram", "manu", "vinni"
+hight1, hight2, hight = 5.8, 4.9, 4.2
+print(student1 , hight1)
+print(student2 , hight2)
+print(student3 , hight)
+
+age = "i am 20 years old"
+print(age)
+
+
+a = input(int())
+b = input(int())
+sum = a+b
+print(sum)
