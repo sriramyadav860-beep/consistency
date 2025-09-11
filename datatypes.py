748e90ffba47aa38ca91ea069c6b3e72755c381e
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-09-11 22:51:16 +0530

    Your message about the changes

--- datatypes.py
+++ datatypes.py
@@ -1,76 +1,22 @@
-decimal_num = 5.5
-integer_num = int(decimal_num)
-print("decimal",decimal_num)
-print("integer",integer_num)
+"hello" + "world"
 
-a = 5.4
-b = int(a)
-print("deci",a)
-print("int",b)
-
-a = "sriram"
-b = 7
-c = 5.5
-print("the type of 'a",type(a))
-print("type 'b",type(b))
-print("type 'c",type(c))
+s = "abc"
+result = s * 3
+print(result)  # prints: abcabcabc
 
 
-a = "100"
-b = int(a)
-result = b + 50
-print(result)
+s = "python"
+first_char = s[0]
+last_char = s[-1]
+print(first_char, last_char)  # prints: p n
 
-# Assign an integer to the variable
-x = 42
-print("x as integer:", x)
-
-# Reassign a string to the same variable
-x = "Python"
-print("x as string:", x)
-
-# Reassign a float to the same variable
-x = 3.14159
-print("x as float:", x)
-
-
-
-# Take input from the user
-user_input = input(123)
-
-# Print the input and its data type
-print("You entered:", user_input)
-print("Data type:", type(user_input))
-
-
-
-# Input: two integers
-a = 5
-b = 10
-
-
-# Swapping using tuple unpacking
-a, b = b, a
-
-# Output after swap
-print("After swapping:")
-print("a =", a)
-print("b =", b)
+s = "programming"
+sub = s[:6]   # takes from index 0 up to (but not including) index 6
+print(sub)    # prints: progra
 
 
 
 
-
-
-# Take input from the user
-text = input("sriram ")
-
-# Initialize an empty string for the reversed version
-reversed_text = ""
-
-# Loop through the string in reverse order
-for char in text:
-    reversed_text = char + reversed_text  # Prepend each character
-
-# Print the reversed string
-print("Reversed string:", reversed_text)
+s = "hello"
+reversed_s = s[::-1]
+print(reversed_s)  
