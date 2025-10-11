c3e2e2d666885f15d9a5a6667ecdbfc72564ee54
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-10-11 23:25:05 +0530

    Your message about the changes

--- datatypes.py
+++ datatypes.py
@@ -20,3 +20,31 @@ print(sub)    # prints: progra
 s = "hello"
 reversed_s = s[::-1]
 print(reversed_s)  
+
+
+# Add two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum_result = a + b
+print("The sum is:", sum_result)
+
+
+a = 15
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)          # normal division
+print("Floor Division:", a // b)   # quotient only
+print("Modulus:", a % b)           # remainder
+print("Power:", a ** b)            # a^b
+
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
