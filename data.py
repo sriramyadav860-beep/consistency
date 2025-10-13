b62cf3a5eabd36dd31206c9d4a80b026d2b1718d
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-10-13 17:21:46 +0530

    Your message about the changes

---
+++ data.py
@@ -0,0 +1,27 @@
+num = 12.75        
+num_int = int(num)
+print(num_int)
+
+
+num = 55.66
+int = int(num)
+print(int)
+
+
+value = "sriram"
+print(type(value))
+
+
+value = 55
+print(type(value))
+
+value = 88.55
+print(type(value))
+
+s = "100"          # string
+num = int(s)       # convert to integer
+result = num + 50
+print(result)
+
+
+
--- datatype.py
+++ datatype.py
@@ -1,26 +1,4 @@
-# Add two numbers
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-sum_result = a + b
-print("The sum is:", sum_result)
-
-
-a = 15
-b = 4
-
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)          # normal division
-print("Floor Division:", a // b)   # quotient only
-print("Modulus:", a % b)           # remainder
-print("Power:", a ** b)            # a^b
-
-
-num = int(input("Enter a number: "))
-
-if num % 2 == 0:
-    print("Even Number")
-else:
-    print("Odd Number")
+s = "100"          # string
+num = int(s)       # convert to integer
+result = num + 50
+print(result)
