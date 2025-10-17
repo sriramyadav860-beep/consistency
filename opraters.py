debfc295ff1e91ea3937a6a7a4e76e14d2d8a731
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-10-17 22:47:49 +0530

    Your message about the changes

--- opraters.py
+++ opraters.py
@@ -16,3 +16,25 @@ num = 7
 print( num % 7)
 
 
+a = 5
+b = 9
+c = 7
+
+if a > 0 and b > 0 and c > 0:
+    print("All numbers are positive.")
+else:
+    print("Not all numbers are positive.")
+
+
+num = 9
+num += 10
+print("Result after adding 10:", num)
+
+
+
+a = 9
+b = 8
+
+print("Bitwise AND (&):", a & b)
+print("Bitwise OR (|):", a | b)
+print("Bitwise XOR (^):", a ^ b)
