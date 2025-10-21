edc7e60cf6c07d70e6d5de393f53ff64c7d01ddf
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-10-21 23:08:07 +0530

    Your message about the changes

---
+++ control.py
@@ -0,0 +1,33 @@
+num = 9
+
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+num = 8
+
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+a = 8
+b = 5
+
+if a > b:
+    print(a, "is greater.")
+elif b > a:
+    print(b, "is greater.")
+else:
+    print("Both numbers are equal.")
+
+a = 16
+b = 16
+
+if a == b:
+    print("Both numbers are equal.")
+else:
+    print("The numbers are not equal.")
