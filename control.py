fad9a1f235bd777919813b19a4d61ab49a1f0d8f
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-10-22 23:51:18 +0530

    Your message about the changes

--- control.py
+++ control.py
@@ -40,11 +40,41 @@ else:
 
 
 
-ch = input("4")
+a = input("Enter a character: ")
 
-if ch.isalpha():
-    print(ch, "is an alphabet.")
-elif ch.isdigit():
-    print(ch, "is a digit.")
+if a.isalpha():
+    print(a, "is an alphabet.")
+elif a.isdigit():
+    print(a, "is a digit.")
 else:
-    print(ch, "is a special character.")
+    print(a, "is a special character.")
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+if a >= b and a >= c:
+    print(a, "largest")
+elif b >= a and b >= c:
+    print(b, "largest")
+else:
+    print(c, "largest")
+
+
+num = int(input("Enter a number: "))
+
+if num % 5 == 0 and num % 11 == 0:
+    print(num, "is divisible  5 and 11.")
+else:
+    print(num, "is not divisible  5 and 11.")
+
+
+num = 77
+
+if num >=10 and age <=99:
+    print(num, "is a two-digit number.")
+else:
+    print(num, "is not a two-digit number.")
+
+
