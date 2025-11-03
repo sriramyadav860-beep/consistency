230af4924c59cedfcfcd591155a1d4ae31664226
sriramyadav860-beep <sriramyadav860@gmail.com>
2025-11-03 22:59:24 +0530

    Your message about the changes

--- string.py
+++ string.py
@@ -1,10 +1,4 @@
-
-
-
-units = 220
-if units <= 100:
-    print( 5*units)
-elif units >= 100 and units <= 199:
-    print(8*units)
-elif units  >= 200:
-    print(10*units)
+str1 = "sri"
+str2 = "ram"
+result = str1 + str2
+print(result)
